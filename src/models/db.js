7b6a04e1607bbe1ec96c3b6cbbc58e7c9e1628fb
HubCatalinasCenter <consultasnoc@ctl.com.ar>
2023-05-16 18:16:32 -0300

    fixed messages error

--- src/models/db.js
+++ src/models/db.js
@@ -9,9 +9,9 @@ mongoose.connect(`mongodb+srv://cristianrios:CoderHouse2023@coderhouse.z6tbriz.m
 const db = mongoose.connection;
 
 
-db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.on('error', console.error.bind(console, 'Error to connect MongoDB:'));
 db.once('open', () => {
-  console.log('Conexión exitosa a MongoDB');
+  console.log('Connection succesfully to  MongoDB');
 });
 
 export default  db;
